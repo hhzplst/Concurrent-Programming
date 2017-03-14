b2c5b7c3f0c8a59b58b0ebdc20d51053bd46972d
Ivy Chang <ivycwq@icloud.com>
2017-03-14 14:41:49 -0700

    setting up indices

--- ConcurrentProgramming.java
+++ ConcurrentProgramming.java
@@ -1,3 +1,19 @@
 public class ConcurrentProgramming {
-  
+  private int lo, hi;
+
+  synchronized public int getLow() {
+    return lo;
+  }
+
+  synchronized public void setLow(int newLo) {
+    lo = newLo;
+  }
+
+  synchronized public int getHi() {
+    return hi;
+  }
+
+  synchronized public void setHi(int newHi) {
+    hi = newHi;
+  }
 }
